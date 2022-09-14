d3de8903d3576b5b14e0e4546d4ba8af31db24af
K4MiR <cleberrobson16@gmail.com>
2022-09-14 01:42:52 -0300

    Foi configurado ao firebase, alterado icon do app e nome

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:frasesonday/screen/Menu_Screen.dart';
 
@@ -5,7 +6,7 @@ import 'package:frasesonday/screen/Menu_Screen.dart';
 
 Future main() async  {
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp( MyApp());
 }
 
